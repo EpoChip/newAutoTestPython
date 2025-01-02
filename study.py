80315969f8f7e66d97fc71e149a7720871bc7031
Epo Chep <epchep23@mail.ru>
2025-01-02 21:25:08 +0300

    коммит вечерние повторение

---
+++ study.py
@@ -0,0 +1,23 @@
+
+# типы данных
+int_primer = 5 # int
+float_primer = 5.34 # float
+str_primer = "строка" # str
+bool_primer = True # bool
+bool_primer2 = False # bool
+list_primer = ["23", 1331, "dadad", 3.3]# список
+tuple_primer = (2332, "daddad", 131.23) # кортеж
+set_primer = {323, 323, "dadad", 32.22, "adad"} # множество
+dict_primer = {"one": 'eefsf', 'two': 'sfs'} # словарь
+
+print(dict_primer)
+
+
+
+# Порядок операций
+# 1. Parentheses - Скобки
+# 2. Exponentiation - Возведение в степень
+# 3. Multiplication - Умножение
+# 4. Division - Деление
+# 5. Addition - Сложение
+# 6. Subtraction - Вычитания
